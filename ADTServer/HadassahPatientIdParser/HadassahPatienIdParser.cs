cb06c3d926cf50b88be61253f1f7acfd55da7304
moshe krichely <moshekri@gmail.com>
2019-12-21 09:55:40 -0800

    added hadassah support

---
+++ ADTServer/HadassahPatientIdParser/HadassahPatienIdParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdtSvrCmn.Interfaces;
+using AdtSvrCmn.Objects;
+
+namespace HadassahPatientIdParser
+{
+    class HadassahPatienIdParser : IPatientIdHandler
+    {
+        public PatientId[] ParseID(string idToParse)
+        {
+
+            int sifratBikret = 0;
+            int.TryParse(idToParse[idToParse.Length - 1].ToString(), out sifratBikret);
+
+            PatientId id1 = new PatientId() { ID = idToParse, IsValidIsraeliId = true, SifratBikuret = sifratBikret.ToString(), SugId = idToParse[0].ToString() };
+            return new PatientId[] { id1, id1 };
+        }
+    }
+}
---
+++ ADTServer/HadassahWebServiceDataClient/HadassahPatientDataSource.cs
@@ -0,0 +1,89 @@
+﻿using AdtSvrCmn.Interfaces;
+using AdtSvrCmn.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HadassahWebServiceDataClient.HadassahWebClient;
+using System.Configuration;
+
+namespace HadassahWebServiceDataClient
+{
+    class HadassahPatientDataSource : IPatientInfoSoapSource
+    {
+        public event EventHandler ErrorGettingPatientInfo;
+        public event EventHandler RecevingPatientInfo;
+        public event EventHandler RequestingPatientInfo;
+
+        public CompletePatientInformation GetPatientInfo(string CustumerId)
+        {
+            demogByPatientIDResponseType response;
+            using (var client = new HadassahWebClient.DWHDemogPortTypeClient("DWHDemogPortTypeEndpoint0SSL"))
+            {
+
+                demogByPatientIDRequestType req = new demogByPatientIDRequestType();
+                req.RequestorID = ConfigurationManager.AppSettings["ReqSysID"];
+                req.PatientID = CustumerId;
+                response = client.GetDemogByPatientID(req);
+            }
+            var dateOfBirth = response.PatientDemography.BirthDate.Split('/');
+            
+            string age = CalculateAge(response.PatientDemography.BirthDate);
+            string gender = GetGender(response.PatientDemography.Sex);
+            string dob = dateOfBirth[2] + dateOfBirth[1] + dateOfBirth[0];
+
+            return new CompletePatientInformation()
+            {
+                Age = age,
+                Gender = gender,
+                DOB = dob,
+                FirstName = response.PatientDemography.FirstName,
+                LastName = response.PatientDemography.LastName ,
+                Height = "",
+                PatientId = CustumerId,
+                GenderDesc = response.PatientDemography.Sex,
+                ResponseStatusMessage = response.Result,
+                CompleteResponseStatusMessage = response.ReturnedException.Exception.ExecptionText,
+                Severity = response.ReturnedException.Exception.SevirityLevel,
+                ResponseStatus = response.Result
+               
+            };
+        }
+
+        private string GetGender(string sex)
+        {
+            sex = sex.ToLower();
+            switch (sex)
+            {
+                case "ז":
+                    return "M";
+                case "נ":
+                    return "F";
+                case "ב":
+                    return "";
+                default:
+                    return "";
+            }
+        }
+
+        private string CalculateAge(string birthDate)
+        {
+            var dateElements = birthDate.Split('/');
+            DateTime bDate = new DateTime(int.Parse(dateElements[2]), int.Parse(dateElements[1]), int.Parse(dateElements[0]));
+            int years = (int)((DateTime.Now - bDate).TotalDays / 365);
+            return years.ToString();
+        }
+
+        public CompletePatientInformation GetPatientInfo(string CustumerId, string pidType)
+        {
+            return GetPatientInfo(CustumerId);
+        }
+
+        public CompletePatientInformation GetPatientInfo(PatientId patientId)
+        {
+            return GetPatientInfo(patientId.ID);
+
+        }
+    }
+}
